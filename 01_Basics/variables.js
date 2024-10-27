806d2f425c7c38802f36eeb2ebe3da457bc98eed
Gaurav kive <kive.gaurav@gmail.com>
2024-10-27 19:06:35 +0000

    variables walkthrough

---
+++ 01_Basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = "1458"
+let employeeEmail = "abc@gmail.com"
+var employeePassword = "821"
+accountType = "Employee"
+let accountState;
+
+//accountId = "321"
+employeeEmail = "jammu@gmail.com"
+employeePassword = "001"
+accountType = "Manager"
+
+/*console.log(accountId);
+console.log(employeeEmail);
+console.log(employeePassword);
+console.log(accountType); */
+
+console.log([accountId, employeeEmail, employeePassword, accountType, accountState]);
+
+
+
+
+
