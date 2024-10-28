168cf07adb174c28f8bfa2705e511b320455e538
Gaurav kive <kive.gaurav@gmail.com>
2024-10-28 11:44:04 +0000

    datatypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,15 @@
+let strValue = "Gaurav"
+let age = 19
+let isServer = true
+let dbl = 45.897
+
+
+console.log(strValue)
+console.log(age)
+console.log(isServer)
+console.log(dbl)
+
+console.log(typeof strValue)
+console.log(typeof age)
+console.log(typeof isServer)
+console.log(typeof dbl)
