97f0394fb9b0d1f8a576a7769ad56e5af9c977b8
Gaurav kive <kive.gaurav@gmail.com>
2024-10-29 13:49:10 +0000

    data conversion

---
+++ 01_Basics/dataConversion.js
@@ -0,0 +1,48 @@
+let score = 33
+let nm = "hrishi"
+let dblVal = 99.99
+let retval = 1
+let boolVal = true
+miscVal = 8789
+
+
+// Before chsnged
+/*console.log(typeof score)
+console.log(typeof nm)
+console.log(typeof dblVal)
+console.log(typeof retval)
+console.log(typeof boolVal)
+console.log(typeof miscVal)*/
+
+// verfied after Convert datatype 
+
+let changedScore = String(score)
+let changednm = Number(nm)
+let changeddblVal = String(dblVal)
+let changedretval = Boolean(retval)
+let changedboolVal = Number(boolVal)
+let changedmiscVal = Boolean(miscVal)
+
+
+
+console.log(changedScore)
+console.log(changednm)
+console.log(changeddblVal)
+console.log(changedretval)
+console.log(changedboolVal)
+console.log(changedmiscVal)
+
+
+console.log(typeof changedScore)
+console.log(typeof changednm)
+console.log(typeof changeddblVal)
+console.log(typeof changedretval)
+console.log(typeof changedboolVal)
+console.log(typeof changedmiscVal)
+
+
+
+
+
+
+
