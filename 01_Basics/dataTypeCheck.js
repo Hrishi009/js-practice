eee2c017002850ba2f9bf740a640f2495aa73baf
Gaurav kive <kive.gaurav@gmail.com>
2024-11-03 18:47:23 +0000

    dataTypeCheck

---
+++ 01_Basics/dataTypeCheck.js
@@ -0,0 +1,48 @@
+//Primitive data types
+let score = 3
+let strMsg = "Hrishi"
+let retVal = false
+let retScore = null
+let floatVal = 102.3
+let notVal 
+
+//Non primitive data types reference types
+
+let arr = ["name","rollNo","subject"]
+let scoreSymbol_1 = Symbol("123")
+let scoreSymbol_2 = Symbol("123")
+
+let funcVal = {
+    name: "Hrishi",
+    rollNo: 104.9
+} 
+
+let func_2 = function() {
+    console.log("inside function")
+}
+
+console.log(typeof score)
+console.log(typeof strMsg)
+console.log(typeof retVal)
+console.log(typeof retScore)
+console.log(typeof floatVal)
+
+console.log(typeof scoreSymbol_1)
+console.log(typeof scoreSymbol_2)
+console.log(typeof arr)
+console.log(typeof funcVal)
+console.log(typeof func_2)
+console.log(typeof notVal)
+console.log(typeof retScore)
+
+
+
+
+
+
+
+
+
+
+
+
