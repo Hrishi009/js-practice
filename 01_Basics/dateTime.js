a843652a54d40fb909f8e82522569e38f2d29308
Gaurav kive <kive.gaurav@gmail.com>
2025-03-19 09:55:25 +0000

    Date time stuff

---
+++ 01_Basics/dateTime.js
@@ -0,0 +1,16 @@
+let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleDateString())
+
+let formattedDt = new Date(2023,0,14)
+//console.log(formattedDt.toDateString())
+
+let formattedDt2 = new Date("2023-01-14")
+//console.log(formattedDt2.toDateString())
+
+let currentDt =  Date.now()
+console.log(currentDt)
+
