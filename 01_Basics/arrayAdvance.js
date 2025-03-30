90b74c994f24bea1183977b8629383e8f4b8adee
Gaurav kive <kive.gaurav@gmail.com>
2025-03-30 07:12:42 +0000

    array Advanced completed

--- 01_Basics/arrayAdvance.js
+++ 01_Basics/arrayAdvance.js
@@ -2,16 +2,16 @@ let arrayFirst = ["hitesh","Ramesh"]
 let arraySecond = ["himanshu","shivam"]
 
 arrayFirst.push(arraySecond)
-console.log(arrayFirst)
+//console.log(arrayFirst)
 
 let arrayCocatinated = arrayFirst.concat(arraySecond)
-console.log(arrayCocatinated)
+//console.log(arrayCocatinated)
 
 let newArray = [1,2,3,[4,5,6,7],[8,[9,[10,11,[12,13,14]]]],15,16,17]
 
 let flatArray = newArray.flat(Infinity)
 
-console.log(flatArray)
+//console.log(flatArray)
 
 let arrayScore1 = [1,2,3,4]
 let arrayScore2 = [5,6,7]
@@ -20,6 +20,18 @@ let arrayScore3 = [8,9,10]
 let spreadedArray = [...arrayScore1,...arrayScore2,...arrayScore3]
 
 console.log(spreadedArray)
+
+let str = "Hitesh"
+
+// console.log(Array.isArray(str))
+// console.log(Array.from(str))
+
+console.log(Array.from({name:"Hitesh"})) // it will give empty array bcz not getting what to do
+
+console.log(Array.of(arrayScore1,arrayScore2,arrayScore3))
+
+
+console.log(Array.of(...arrayScore1,...arrayScore2,...arrayScore3)) // Three dots will be spraed operator
 
 
 
