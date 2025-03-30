fefd8bfae229357c4fe2c1303148f8d5873a970e
Gaurav kive <kive.gaurav@gmail.com>
2025-03-30 08:20:07 +0000

    Object Advanced completed

---
+++ 01_Basics/objectsBeginer.js
@@ -0,0 +1,50 @@
+// objects are decalraed as singlton , literal and constructor. Singlton objects are declared as constructor only
+
+
+const mySymbol = Symbol("keyFirst")
+
+// This is simple objct literal type
+const objFirst = {
+name: "Hitesh",
+[mySymbol]: "Symbolical Key", // This is proper way to inset a symbol as a key without bracket it willbe consider as simple string 
+email: "hitesh@google.com",
+location: "Indore",
+age: 18,
+lastLoggedIn: false
+} 
+
+//console.log(objFirst)
+// console.log(objFirst.email)
+// console.log(objFirst["email"])
+//console.log(objFirst.mySymbol) // it is not considering symbol actually
+
+//console.log(typeof(objFirst[mySymbol]))
+
+
+objFirst.email = "hitesh@hercircle.com"
+//console.log(objFirst.email)
+
+
+// If you want to freez ur object that nothing change should visible on your object.
+
+//Object.freeze(objFirst)
+
+objFirst.email = "hitesh@google.com" // this will never change bcz of freez object earlier
+//console.log(objFirst.email)
+
+
+//console.log(objFirst)
+
+//Now adding a function 
+
+objFirst.greeting = function(){
+    console.log("This is greeting function");
+}
+
+objFirst.greetingTwo = function(){
+    console.log(`This is greeting function for, ${this.email}`);
+}
+
+console.log(objFirst.greeting())
+console.log(objFirst.greetingTwo())
+
