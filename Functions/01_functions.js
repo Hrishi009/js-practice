05e239e69775f90cabb1ed94a34f41f57a69b77d
Gaurav kive <kive.gaurav@gmail.com>
2025-05-15 08:14:48 +0000

    Functions overview

---
+++ Functions/01_functions.js
@@ -0,0 +1,13 @@
+function sayString(str1 ,str2 ){
+   // console.log("The world")
+   // console.log("wants you.")
+   let result = str1 + str2
+    return result
+
+}
+
+//sayString()
+
+let result  = sayString("The Home","Stay")
+
+console.log(result)
