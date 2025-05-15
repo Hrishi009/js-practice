b7b2b3f421fa64df8ffd7b0ed6a9f40abcd18dc0
Gaurav kive <kive.gaurav@gmail.com>
2025-05-15 09:20:26 +0000

    funtion understanding

--- Functions/01_functions.js
+++ Functions/01_functions.js
@@ -2,6 +2,8 @@ function sayString(str1 ,str2 ){
    // console.log("The world")
    // console.log("wants you.")
    let result = str1 + str2
+    //let result = '${str1} + ${str2}'
+
     return result
 
 }
@@ -10,4 +12,17 @@ function sayString(str1 ,str2 ){
 
 let result  = sayString("The Home","Stay")
 
-console.log(result)
+//console.log(result)
+
+
+function paramFunction (userNm = "Developer"){
+  if (!userNm){
+    console.log("please enter correct name")
+    return
+  }
+  return `${userNm} logged in`
+}
+
+//paramFunction("Developer")
+
+console.log(paramFunction("Hitesh"))
