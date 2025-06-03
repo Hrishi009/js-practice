8a2e7683161b30415e3a407a46a74167733cf105
Gaurav kive <kive.gaurav@gmail.com>
2025-06-03 18:33:53 +0000

    function scop and function assigning in a variable

--- Scop/01_Basics.js
+++ Scop/01_Basics.js
@@ -7,9 +7,39 @@
 if(true){
 let a = 10
 const b =  20
-c = 300 
+//var c = 300 
+c = 300
 }
 
 //console.log(a)
 //console.log(b)
-console.log(c)
+//console.log(c)
+
+function one(){
+    const name = "Dev"
+    function two(){
+        const website = "You tube"
+        console.log(name)
+    }
+    //console.log(website) // outside scop
+    two()
+}
+//console.log(name) // outside scop
+
+one()
+
+
+console.log(addOne(3))
+
+function addOne(num){
+    return num + 1
+}
+
+//console.log(holdingVariable(4)) // here it will give error Cannot access 'holdingVariable' before initialization
+
+
+const holdingVariable = function twoadd(num){
+    return num + 3
+}
+
+console.log(holdingVariable(4))
